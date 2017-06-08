cb2f6e37a8a5a339896e8c3e9a909068939aaf06
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-07 22:20:37 -0700

    Add speaking response functionality

--- chatbot/chat.js
+++ chatbot/chat.js
@@ -10,7 +10,7 @@ var accessToken = "86bbaa261369454894e364da26cbebe1",
     messageCouldntHear = "I couldn't hear you, can you talk louder?",
     messageInternalError = "Uh oh--internal server error",
     messageSorry = "Apologies, I don't have an answer"
-    ;
+    botName = "protoBot";
    
 //typing
 $speechInput.keypress(function(event) {
@@ -77,3 +77,61 @@ function switchRecognition() {
         startRecognition();
     };
 };
+
+
+/***************************************/
+//communicate with Api.Ai
+//standard AJAX POST reuqest using jquery
+//validate we're sending JSON data
+function send() {
+    var text = $speechInput.val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "query",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({query: text, lang: "en", sessionId: "run" + botName}),
+        success: function(data) {
+            prepareResponse(data); //format the response from API AI
+        },
+        error: function() {
+            respond(messageInternalError);
+        }
+    });
+};
+
+function prepareResponse(val) {
+    var debugJSON = JSON.stringify(val, undefined, 2);
+    var spokenResponse = val.result.speech;
+    //TBD what this is for--input or output?
+    
+    respond(spokenResponse);
+    debugRespond(debugJSON);
+};
+
+function respond(val) {
+    //API AI unable to respond -- failed to return a valid response
+    if (val == "") {
+        val = messageSorry;
+    };
+    //
+    if (val !== messageRecording) { //don't have the page speak that the page is "recording/listening to the user"
+        var msg = new SpeechSynthesisUtterance();
+        var voices = window.speechSynthesis.getVoices();
+        msg.voiceURI = "native";
+        msg.text = val;
+        msg.lang = "en-US";
+        window.speechSynthesis.speak(msg);
+    };
+    $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+};
+
+
+
+//put text into your field for a JSON response
+function debugRespond(val) {
+    $("#response").text(val);
+}
