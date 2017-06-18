62abb22b36119b571c4ca82205dc2c4c0a0a2c5c
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-17 17:50:22 -0700

    Add response to You Are JJ

--- dev/cbot/generate_reply.py
+++ dev/cbot/generate_reply.py
@@ -12,9 +12,17 @@ BOT_GREETINGS = ["Greetings, my friend", "Hello, sir"]
 # Generates a bot response from a user message
 def generateReply(message):
     tokens = message.split(" ")
+    message_pos = getPOS(message) 
+    sentiment = getSentiment(message)
     if len(tokens) > 0 and tokens[0].lower() in USER_GREETINGS:
         return respondToGreeting(message)
     
+    youAreJJ = findYouAreJJ(message_pos)
+    if (youAreJJ != False):
+        if sentiment > 0.5:
+            return "Yes, I know I'm " + youAreJJ
+        else:
+            return "How dare you call me " + youAreJJ
     return respondToSentiment(message)
     # Otherwise the bot doesn't understand what the user said
     #return "I don't understand." 
@@ -22,9 +30,7 @@ def generateReply(message):
 def respondToGreeting(message):
     return random.choice(BOT_GREETINGS)
     
-def respondToSentiment(message):
-    sentiment = getSentiment(message)
-
+def respondToSentiment(sentiment, message):
     if sentiment >= 0.7:
         return respondPositive(message)
     elif sentiment >= 0.3:
@@ -36,7 +42,7 @@ def respondPositive(message):
     bot_positive = ["I'm happy to hear that", "That's awesome, my friend"]
     return random.choice(bot_positive)
     
-def respondNeutral(message);
+def respondNeutral(message):
     bot_neutral = ["You seem measured", "You seem neutral"]
     return random.choice(bot_neutral)
 
@@ -44,7 +50,25 @@ def respondNegative(message):
     bot_negative = ["That's too bad", "Go away, you're being negative"]
     return random.choice(bot_negative)
     
+    
+def findYouAreJJ(message_pos):
+    tokenIndex = 0 
+    posIndex = 1
+    
+    foundYou = False 
+    foundYouAre = False 
+    for p in message_pos:
+        if p[tokenIndex].lower() == "you":
+            foundYou = True 
+        elif p[tokenIndex].lower() == "are":
+            foundYouAre = True 
+        if foundYou and foundYouAre and p[posIndex] == "JJ":
+             return p[tokenIndex]
+    return False 
+ 
 '''
 Testing 
 print(respondToSentiment("I hate you"))
+print(generateReply("You are smart"))
+print(generateReply("You are crass"))
 '''
