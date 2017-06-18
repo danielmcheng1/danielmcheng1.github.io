415e4c9f392d9e25d9ab0e04467b19d46bd86cdf
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-17 17:39:37 -0700

    Add basic sentiment response

--- dev/cbot/generate_reply.py
+++ dev/cbot/generate_reply.py
@@ -4,12 +4,10 @@ import traceback
 
 #own modules 
 from sentiment import getSentiment
+from linguistic import getPOS 
 
 USER_GREETINGS = ["greetings", "hi", "hello", "hey", "howdy", "whatsup", "sup"]
 BOT_GREETINGS = ["Greetings, my friend", "Hello, sir"]
-BOT_POSITIVE = ["I'm happy to hear that", "That's awesome, my friend"]
-BOT_NEGATIVE = ["That sucks", "That's too bad"]
-BOT_NEUTRAL = ["You seem measured"]
 
 # Generates a bot response from a user message
 def generateReply(message):
@@ -28,11 +26,24 @@ def respondToSentiment(message):
     sentiment = getSentiment(message)
 
     if sentiment >= 0.7:
-        return random.choice(BOT_POSITIVE)
+        return respondPositive(message)
     elif sentiment >= 0.3:
-        return random.choice(BOT_NEUTRAL)
+        return respondNeutral(message)
     else:
-        return random.choice(BOT_NEGATIVE)
+        return respondNegative(message)
+        
+def respondPositive(message):
+    bot_positive = ["I'm happy to hear that", "That's awesome, my friend"]
+    return random.choice(bot_positive)
+    
+def respondNeutral(message);
+    bot_neutral = ["You seem measured", "You seem neutral"]
+    return random.choice(bot_neutral)
+
+def respondNegative(message):
+    bot_negative = ["That's too bad", "Go away, you're being negative"]
+    return random.choice(bot_negative)
+    
 '''
 Testing 
 print(respondToSentiment("I hate you"))
