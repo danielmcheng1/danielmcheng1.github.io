975894e3b1eda42714ca7f277d5c098322ca03bc
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-17 17:14:19 -0700

    Add basic greeting response

--- dev/cbot/generate_reply.py
+++ dev/cbot/generate_reply.py
@@ -1,3 +1,15 @@
+import random 
+
+USER_GREETINGS = ["greetings", "hi", "hello", "hey", "howdy", "whatsup", "sup"]
+BOT_GREETINGS = ["greetings my friend", "hello, sir"]
+
 # Generates a bot response from a user message
 def generateReply(message):
+    tokens = message.split(" ")
+    if len(tokens) > 0 and tokens[0].lower() in USER_GREETINGS:
+        return respondToGreeting(message)
+     
     return "I don't understand." # Otherwise the bot doesn't understand what the user said
+    
+def respondToGreeting(message):
+    return random.choice(BOT_GREETINGS)
