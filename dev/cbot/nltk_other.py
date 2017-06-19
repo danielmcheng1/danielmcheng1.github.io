18181055e9ece942e3a679a5f330332365803473
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-18 23:10:53 -0700

    Fix bug with POS converter not returning anny result

---
+++ dev/cbot/nltk_other.py
@@ -0,0 +1,45 @@
+#Source: http://nlpforhackers.io/convert-words-between-forms/ 
+
+from nltk.corpus import wordnet as wn
+ 
+WN_NOUN = 'n'
+WN_VERB = 'v'
+WN_ADJECTIVE = 'a'
+WN_ADJECTIVE_SATELLITE = 's'
+WN_ADVERB = 'r'
+
+def convertPOS(word, from_pos, to_pos):    
+    """ Transform words given from/to POS tags """
+ 
+    synsets = wn.synsets(word, pos=from_pos)
+    # Word not found
+    if not synsets:
+        return [(word, 0.0)]
+    # Get all lemmas of the word (consider 'a'and 's' equivalent)
+    #Python 3--update s.lemmas and s.name to be method calls instead of attributes
+    lemmas = [l for s in synsets
+                for l in s.lemmas() 
+                if s.name().split('.')[1] == from_pos
+                    or from_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)
+                        and s.name.split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)]
+ 
+    # Get related forms
+    derivationally_related_forms = [(l, l.derivationally_related_forms()) for l in lemmas]
+ 
+    # filter only the desired pos (consider 'a' and 's' equivalent)
+    #Python 3--update l.sysnet and .name to be method calls instead of attributes
+    related_noun_lemmas = [l for drf in derivationally_related_forms
+                             for l in drf[1] 
+                             if l.synset().name().split('.')[1] == to_pos
+                                or to_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)
+                                    and l.synset().name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)]
+    # Extract the words from the lemmas
+    words = [l.name() for l in related_noun_lemmas]
+    len_words = len(words)
+ 
+    # Build the result in the form of a list containing tuples (word, probability)
+    result = [(w, float(words.count(w))/len_words) for w in set(words)]
+    result.sort(key=lambda w: -w[1])
+ 
+    # return all the possibilities sorted by probability
+    return [(word, 0.0)] if result == [] else result 
