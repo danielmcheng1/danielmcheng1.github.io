2562300e5ef94301432bfc8ce75ae4cedc7618c8
danielmcheng1 <dcheng21@uchicago.edu>
2017-06-21 23:08:16 -0700

    Add flask environment for Twilio integration

---
+++ dev/cbot/twilio_sandbox.py
@@ -0,0 +1,27 @@
+#from twilio.rest import Client
+from flask import Flask, request, redirect
+#from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = MessagingResponse().message("Hello, Mobile Monkey")
+    return str(resp)
+
+def send_message(message, to_number):    
+    # Find these values at https://twilio.com/user/account
+    account_sid = "AC50c2f1517cb7744e5a71b56e9bdd013b"
+    auth_token = "4a5718f69a6fe0f4da633ecd073674f0"
+    client = Client(account_sid, auth_token)
+    from_twilio = "+12175744267"
+    message = client.api.account.messages.create(to=to_number,
+                                                 from_=from_twilo,
+                                                 body=message)
+                                                 
+if __name__ == "__main__":
+    app.run(debug=True)
+    print("hello from twilio")
+    #send_message("hello again", "+12177227216")
