91e504da22faad42e4505cf9080765f8099249ed
danielmcheng1 <dcheng21@uchicago.edu>
2017-07-08 04:06:47 -0500

    Test ability to pass in array to display on front end

--- dev/scrabble/static/js/run_game.js
+++ dev/scrabble/static/js/run_game.js
@@ -5,14 +5,39 @@ BOARD_MIN_COL = 0;
 BOARD_MAX_COL = 15;
 BOARD_CENTER_ROW = 7;
 BOARD_CENTER_COL = 7;
-
+var INPUT_BOARD_TILES = [
+    [ 'T', 'H', 'E', '', 'Q', 'U', 'I', 'C', 'K', '', '', '', '', '', ''],
+    ['B', 'R', 'O', 'W', 'N', '', 'F', 'O', 'X', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    
+    
+    ['', 'J', 'U', 'M', 'P', 'E', 'D', '', 'O', 'V', 'E', 'R', '', '', ''],
+    
+    ['', '', '', 'T', 'H', 'E', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', 'L', 'A', 'Z', 'Y'],
+    ['', '', '', '', '', '', '', 'S', 'L', 'E', 'E', 'P', 'I', 'N', 'G'],
+    ['', '', '', '', '', 'D', 'O', 'G', 'S', '', '', '', '', '', '']
+];
 var table_whole = "";
-for (var i = 0; i <= BOARD_MAX_ROW; i++) {
+for (var i = 0; i < BOARD_MAX_ROW; i++) {
     var table_row = "<tr>";
-    for (var j = 0; j <= BOARD_MAX_COL; j++) {
+    for (var j = 0; j < BOARD_MAX_COL; j++) {
         var table_cell = '<td class=boardCells id=board_' + i + '_' + j + '>';
-        if ((i == 1 || i == 5 || i == 9) && (j == 1 || j == 3 || j == 11)) {
-            var tile = '<span class="tileOnBoard tileUnselected tileComputer">S<sub class="tilePoints">1</sub></span>'; 
+        console.log(INPUT_BOARD_TILES, i , j)
+        var letter = INPUT_BOARD_TILES[i][j];
+        if (letter != '') {
+            if (j % 2) 
+                var player = 'Computer';
+            else 
+                var player = 'Human';
+            tile = '<span class="tileOnBoard tileUnselected tile' + player + '">' + letter + '<sub class="tilePoints">1</sub></span>'; 
             table_cell = table_cell + tile;         
         };
         table_cell = table_cell + '</td>';
@@ -24,5 +49,6 @@ for (var i = 0; i <= BOARD_MAX_ROW; i++) {
 $("#board").append(table_whole);
 
 $(".tileOnBoard").click(function () {
-    $(this).toggleClass('tileUnselected tileSelected');
+    if ($(this).hasClass('tileHuman'))
+        $(this).toggleClass('tileUnselected tileSelected');
 });
